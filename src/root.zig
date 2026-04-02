4da38b0f7cdf0cb1b39c6408a2e171d7fce17a06
ivansantiagojr <ivansantiago.junior@gmail.com>
2026-04-01 22:21:51 -0300

    No reason for the tokenizer to be exported yet

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
-pub const tokenizer = @import("tokenizer.zig");
+const tokenizer = @import("tokenizer.zig");
 pub const formulas = @import("formulas.zig");
 
 test {
